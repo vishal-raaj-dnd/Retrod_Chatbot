dca9b73e4041116270bfac911c11d1385131877c
vishal-raaj-dnd <vishalraajdnd@gmail.com>
2026-07-24 08:50:28 +0530

    Add PostgreSQL support via dj-database-url and psycopg2

--- retrod_backend/settings.py
+++ retrod_backend/settings.py
@@ -80,14 +80,17 @@ TEMPLATES = [
 WSGI_APPLICATION = "retrod_backend.wsgi.application"
 
 
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
